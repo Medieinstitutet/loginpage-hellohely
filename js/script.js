8f171da54f8a487c41dcdbb0f97b8518e519adbc
hellohely <52459904+hellohely@users.noreply.github.com>
2021-12-04 16:17:17 +0100

    Created basic html elements and eventlistener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("test")
+console.log("test");
+
+// Notes to self:
+// Localstorage: Använd stringify och parce. 
+// Stringify: JSON.stringify(); 
+// Localstorage kan BARA spara strings!
+
+// Local Storage: Vi hämtar,vi ändrar, vi sparar
+
+let loginBtn = document.createElement("button");
+loginBtn.id = "loginBtn";
+loginBtn.innerHTML = "Logga in";
+document.getElementById("login").append(loginBtn);
+
+loginBtn.addEventListener("click", function () {
+    console.log("Inloggad");
+
+    let userPage = document.createElement("div");
+    userPage.id = "userPage";
+    userPage.innerHTML = "<p> Du är nu inloggad, välkommen!</p>";
+    document.getElementById("login").append(userPage);
+    document.getElementById("loginBtn").innerHTML = "Logga ut";
+});
